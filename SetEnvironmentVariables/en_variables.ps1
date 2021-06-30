69e8d6e0ad3d064c4dd1aa17956d753c8af65449
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-06-30 16:57:27 +0300

    Create en_variables.ps1

---
+++ SetEnvironmentVariables/en_variables.ps1
@@ -0,0 +1,2 @@
+[System.Environment]::SetEnvironmentVariable("PublicKey","jskkdijfiosdlf ndsnfjhdsajfkasdflhdlsakdjfajsdhfliausldfhd.skjfidhslaifhuslaenvPK",[System.EnvironmentVariableTarget]::User)
+[System.Environment]::SetEnvironmentVariable('PrivateKey','122kkjfhskksldkfjfhsjjskkfhs883jienvPK',[System.EnvironmentVariableTarget]::Machine)
