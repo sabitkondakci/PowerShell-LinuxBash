8cac0e3d189dd0b5c7113789b6d2bde2e7891900
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-11-28 21:10:53 +0300

    Create GetFilteredDnsInfo.ps1

---
+++ GetFilteredDnsInfo.ps1
@@ -0,0 +1,30 @@
+$emailList='sahibinden.com','hepsiemlak.com','github.com','google.com','gmail.com','yahoo.com','trendyol.com','hepsiburada.com','gittigidiyor.com','haberturk.com','cimri.com','linkedin.com','microsoft.com','whatsapp.com','telegram.com','tumblr.com','yandex.com','mailru.com'
+
+
+Function Get-FilteredDnsInfo
+{
+    [cmdletbinding()]
+    Param ( [string[]]$mailList, [string]$nameAdmin,[string]$primaryServer )
+    
+    Process{
+
+        $mailList =  $mailList | Sort-Object
+            
+        foreach($mail in $mailList)
+        {
+            $output = Resolve-DnsName -Name $mail -Type DNAME |
+                Select Name,PrimaryServer,NameAdministrator |
+                    Where-Object {$_.NameAdministrator.Contains($nameAdmin) -and
+                        $_.PrimaryServer.Contains($primaryServer) }
+
+            if($output -ne $null){
+                $output
+            }
+               
+        }
+    }
+
+}
+
+Get-FilteredDnsInfo -mailList $emailList -nameAdmin "dns" -primaryServer ""
+
