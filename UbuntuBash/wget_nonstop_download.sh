cac04e4d505b4f910b34f0b17477bb288e1c8823
Sabit Kondakçı <kondakcisabit@gmail.com>
2022-02-08 20:47:41 +0300

    Update wget_nonstop_download.sh

--- UbuntuBash/wget_nonstop_download.sh
+++ UbuntuBash/wget_nonstop_download.sh
@@ -1,3 +1,3 @@
-find '/home/fox/weblist.txt' -exec \
+find '/home/fox/weblist.txt' | xargs -I {} \
 wget -U 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17' \
--t 1 --timeout 3 -i {} \; &
+-t 1 --timeout 3 -i {} &
